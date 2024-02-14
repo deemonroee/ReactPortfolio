13145fbf294d71b84ce7a70f526ee1496c932909
deemonroee <131928597+deemonroee@users.noreply.github.com>
2024-02-14 09:12:21 -0800

    updated files

--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import About from './pages/AboutPage';
+import PortfolioPage from './pages/PorfolioPage.jsx';
+import ContactPage from './pages/ContactPage.jsx';
+import ResumePage from './pages/ResumePage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <About/>
+      },
+      {
+        path: '/portfolio',
+        element: <PortfolioPage/>
+      },
+      {
+        path: '/contact',
+        element: <ContactPage/>
+      },
+      {
+        path: '/resume',
+        element: <ResumePage/>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ react-portfolio/src/pages/Portfolio.jsx
@@ -0,0 +1,21 @@
+import { Row, Col } from 'react-bootstrap';
+import Project from '../../../src/components/Project';
+import data from '../../../src/assets/data/projectsData';
+
+export default function Portfolio() {
+  return (
+    <Row >
+      <h2 className="my-3">Portfolio</h2>
+      {}
+      {data.map((project) => (
+        <Col key={project.id} sm={12} md={6} lg={4} xl={3}>
+          <Project
+            title={project.title}
+            image={project.image}
+            github={project.github}
+          />
+        </Col>
+      ))}
+    </Row>
+  );
+}
